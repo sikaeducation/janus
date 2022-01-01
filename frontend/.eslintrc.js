a6d267e18e110701bb5e5fb6c8fd0dfee45f2f15
Kyle Coberly <kyle.coberly@gmail.com>
2021-12-31 18:10:29 -0700

    Add linting

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, {
+      extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    }],
+    'react/react-in-jsx-scope': 'off',
+  },
+};
