92040d917e3b39d006a7672545aa215a385f6270
Kyle Coberly <kyle.coberly@gmail.com>
2022-01-06 19:02:21 -0700

    Typing React Markdown

--- frontend/src/components/AppContent/AppContent.tsx
+++ frontend/src/components/AppContent/AppContent.tsx
@@ -1,4 +1,4 @@
-/* eslint react/destructuring-assignment: "off", react/no-unstable-nested-components: "off", react/no-children-prop: "off", react/jsx-props-no-spreading: "off", @typescript-eslint/no-explicit-any: "off" */
+/* eslint react/destructuring-assignment: "off", react/no-unstable-nested-components: "off", react/no-children-prop: "off", react/jsx-props-no-spreading: "off" */
 
 import "./AppContent.scss";
 import ReactMarkdown from "react-markdown";
@@ -6,18 +6,19 @@ import gfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula as style } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { Link } from "react-router-dom";
+import { ComponentPropsWithoutRef } from "react";
 
 type props = {
   content: string;
 };
 
-function RouterLink(props: any) {
-  return props.href.match(/^(https?:)?\/\//) ? (
+function RouterLink(props: ComponentPropsWithoutRef<"a">) {
+  return props.href?.match(/^(https?:)?\/\//) ? (
     <a target="_blank" href={props.href} rel="noreferrer">
       {props.children}
     </a>
   ) : (
-    <Link to={props.href}>{props.children}</Link>
+    <Link to={props.href || ""}>{props.children}</Link>
   );
 }
 
