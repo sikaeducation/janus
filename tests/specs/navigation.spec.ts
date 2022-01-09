e86b14731da317fc886113fa4f501c1faa51dd89
Kyle Coberly <kyle.coberly@gmail.com>
2022-01-08 23:33:37 -0700

    Update test url matcher

--- tests/specs/navigation.spec.ts
+++ tests/specs/navigation.spec.ts
@@ -3,38 +3,38 @@ import {navigateTo} from "../utilities"
 test("initial navigation", async () => {
   const page = await navigateTo("/")
 
-  expect(page.url()).toBe("http://frontend:3000/websites")
+  expect(page.url()).toContain("/websites")
 })
 
 test("unit navigation", async () => {
   const page = await navigateTo("/")
 
   await page.click(".UnitNavigation li:nth-child(2)")
-  expect(page.url()).toBe("http://frontend:3000/web-apps")
+  expect(page.url()).toContain("/web-apps")
 
   await page.click(".UnitNavigation li:nth-child(1)")
-  expect(page.url()).toBe("http://frontend:3000/websites")
+  expect(page.url()).toContain("/websites")
 });
 
 test("section navigation", async () => {
   const page = await navigateTo("/websites")
 
   await page.click("text=links")
-  expect(page.url()).toBe("http://frontend:3000/websites/websites-1")
+  expect(page.url()).toContain("/websites/websites-1")
 
   await page.click("text=content")
-  expect(page.url()).toBe("http://frontend:3000/websites/websites-1/html-div-span")
+  expect(page.url()).toContain("/websites/websites-1/html-div-span")
 });
 
 test("activity navigation", async () => {
   const page = await navigateTo("/websites/websites-1/html-div-span")
 
   await page.click("text=Next: <div> & <span> Exercises")
-  expect(page.url()).toBe("http://frontend:3000/websites/websites-1/html-div-span-exercises")
+  expect(page.url()).toContain("/websites/websites-1/html-div-span-exercises")
 
   await page.click("text=Next: Block layout")
-  expect(page.url()).toBe("http://frontend:3000/websites/websites-1/css-block-layout")
+  expect(page.url()).toContain("/websites/websites-1/css-block-layout")
 
   await page.click("text=Back to Websites 1")
-  expect(page.url()).toBe("http://frontend:3000/websites/websites-1")
+  expect(page.url()).toContain("/websites/websites-1")
 });
