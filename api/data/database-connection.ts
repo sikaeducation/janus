d596ab2e1b66fc59e84101c589f6bbf1950862a3
Kyle Coberly <kyle.coberly@gmail.com>
2022-01-31 18:02:31 -0700

    Add debug logging for API

--- api/data/database-connection.ts
+++ api/data/database-connection.ts
@@ -3,4 +3,5 @@ import knex from "knex";
 import config from "./knexfile";
 
 const environmentConfig = config[process.env.NODE_ENV || "development"];
+console.log("Using db config", environmentConfig);
 export default knex(environmentConfig);
