803b5801c8b5f73712bf08ffb46cf847fd17b28c
Kyle Coberly <kyle.coberly@gmail.com>
2022-01-31 18:26:38 -0700

    WIP database debugging

--- api/data/database-connection.ts
+++ api/data/database-connection.ts
@@ -3,5 +3,4 @@ import knex from "knex";
 import config from "./knexfile";
 
 const environmentConfig = config[process.env.NODE_ENV || "development"];
-console.log("Using db config", environmentConfig);
 export default knex(environmentConfig);
