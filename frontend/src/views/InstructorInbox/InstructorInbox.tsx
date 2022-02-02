389c57bd0ad0af71e5a307dac0fce00c75b87c87
Kyle Coberly <kyle.coberly@gmail.com>
2022-02-01 22:16:18 -0700

    Add styling to inbox

--- frontend/src/views/InstructorInbox/InstructorInbox.tsx
+++ frontend/src/views/InstructorInbox/InstructorInbox.tsx
@@ -1,15 +1,40 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { format } from "date-fns";
-import { createRef, useContext, useEffect } from "react";
-import { Navigate } from "react-router-dom";
+import { createRef, useContext, useEffect, useRef } from "react";
+import { Link, Navigate } from "react-router-dom";
 import Gravatar from "react-gravatar";
+import { groupBy } from "lodash/fp";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import { performanceContext } from "../../contexts/performance";
 import "./InstructorInbox.scss";
+
+const formatDateTime = (dateTime: string) =>
+  format(new Date(dateTime), "M/d/yy: p");
+
+const formatTime = (dateTime: string) => format(new Date(dateTime), "p");
 
 function getSubmissionComponent(performance: postedPerformance) {
   switch (performance.type) {
     case "view": {
-      return <div>View! Confidence: {performance.payload.confidenceLevel}</div>;
+      const checks = {
+        1: <FontAwesomeIcon icon={faCheckCircle} className="failure" />,
+        2: <FontAwesomeIcon icon={faCheckCircle} className="warning" />,
+        3: <FontAwesomeIcon icon={faCheckCircle} className="success" />,
+      };
+      return (
+        <div className="LearnerSubmission">
+          <Gravatar email={performance.userId} size={60} />
+          <p>
+            {performance.userId} read{" "}
+            <Link to="#something">{performance.postSlug}</Link>.
+          </p>
+          <span className="confidence-level">
+            {checks[performance.payload.confidenceLevel]}
+          </span>
+          <time>{formatTime(performance.createdAt)}</time>
+        </div>
+      );
     }
     case "submission": {
       return (
@@ -25,29 +50,39 @@ export default function InstructorInbox() {
   const { isAuthenticated } = useAuth0();
   const { performances } = useContext(performanceContext);
   const lastMessageRef = createRef<HTMLLIElement>();
+  const isInitialized = useRef<boolean>(false);
+  const performancesByDay = groupBy((performance: postedPerformance) => {
+    return format(new Date(performance.createdAt), "yyyy/MM/dd");
+  })(performances);
+  console.log(performancesByDay);
+
   useEffect(() => {
-    lastMessageRef?.current?.scrollIntoView({ behavior: "smooth" });
-  }, [lastMessageRef]);
+    if (performances.length > 0 && !isInitialized.current) {
+      isInitialized.current = true;
+      lastMessageRef?.current?.scrollIntoView();
+    }
+  }, [performances, isInitialized, lastMessageRef]);
 
   if (!isAuthenticated) return <Navigate replace to="/" />;
-  const formatDateTime = (dateTime: string) =>
-    format(new Date(dateTime), "M/d/yy: p");
 
   return (
     <div className="InstructorInbox">
-      <ul className="submissions">
-        {performances.map((performance) => (
-          <li key={performance.id}>
-            <div className="LearnerSubmission">
-              <Gravatar email={performance.userId} size={60} />
-              <time>{formatDateTime(performance.createdAt)}</time>
-              <span>{performance.postSlug}</span>
-              Check it out: {getSubmissionComponent(performance)}
-            </div>
-          </li>
+      <h1>Student Activity</h1>
+      <div className="submissions">
+        {Object.entries(performancesByDay).map(([date, performanceByDay]) => (
+          <div key={date}>
+            <h2>{format(new Date(date), "eeee, LLLL do")}</h2>
+            <ul>
+              {performanceByDay.map((performance) => (
+                <li key={performance.id}>
+                  {getSubmissionComponent(performance)}
+                </li>
+              ))}
+              <li className="dummy" ref={lastMessageRef} />
+            </ul>
+          </div>
         ))}
-        <li className="dummy" ref={lastMessageRef} />
-      </ul>
+      </div>
     </div>
   );
 }
