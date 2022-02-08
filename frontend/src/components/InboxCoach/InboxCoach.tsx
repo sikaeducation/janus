c1127985647a834a9737f21bebc6dec44c545261
Kyle Coberly <kyle.coberly@gmail.com>
2022-02-08 15:08:58 -0700

    WIP socket debugging

--- frontend/src/components/InboxCoach/InboxCoach.tsx
+++ frontend/src/components/InboxCoach/InboxCoach.tsx
@@ -6,11 +6,9 @@ import InboxCoachPromptForm from "../InboxCoachPromptForm";
 import "./InboxCoach.scss";
 
 export default function CoachInbox() {
-  const [formShouldDisplay, setFormShouldDisplay] = useState<boolean>(true);
-  const [slug, setSlug] = useState<string>("");
   const [prompt, setPrompt] = useState<string>("");
-  const [tags, setTags] = useState<string[]>([]);
   const [tagString, setTagString] = useState<string>("");
+
   const {
     currentBroadcast,
     startInboxPrompt,
@@ -18,20 +16,16 @@ export default function CoachInbox() {
     getCurrentPrompt,
   } = useContext(promptContext);
 
-  const handleEndPrompt = () => {
-    endInboxPrompt();
-    setFormShouldDisplay(true);
-  };
   const handleStartPrompt = (broadcast: rawBroadcast) => {
     const generatedSlug = generateSlug();
-    setSlug(generatedSlug);
     const broadcastWithSlug = {
       ...broadcast,
       generatedSlug,
     };
     startInboxPrompt(broadcastWithSlug);
-    setFormShouldDisplay(false);
-    setTags(broadcastWithSlug.tags?.split(",").map((tag) => tag.trim()) || []);
+  };
+  const handleEndPrompt = () => {
+    endInboxPrompt();
   };
 
   const currentBroadcastRequested = useRef<boolean>(false);
@@ -40,35 +34,24 @@ export default function CoachInbox() {
       getCurrentPrompt();
       currentBroadcastRequested.current = true;
     }
-  }, [getCurrentPrompt]);
-
-  const currentBroadcastInitialized = useRef<boolean>(false);
-  useEffect(() => {
-    if (currentBroadcast && currentBroadcastInitialized.current === false) {
-      setSlug(currentBroadcast.slug || "");
-      setTagString(currentBroadcast.tags || "");
-      setPrompt(currentBroadcast.prompt || "");
-      setFormShouldDisplay(false);
-      currentBroadcastInitialized.current = true;
-    }
-  }, [currentBroadcast]);
+  });
 
   return (
     <div className="InboxCoach">
-      {formShouldDisplay ? (
+      {currentBroadcast ? (
+        <InboxCoachPromptDisplay
+          slug={currentBroadcast.slug || ""}
+          endPrompt={handleEndPrompt}
+          tags={currentBroadcast.tags?.split(",") || []}
+          prompt={currentBroadcast.prompt}
+        />
+      ) : (
         <InboxCoachPromptForm
           startPrompt={handleStartPrompt}
           tagString={tagString}
           setTagString={setTagString}
           prompt={prompt}
           setPrompt={setPrompt}
-        />
-      ) : (
-        <InboxCoachPromptDisplay
-          slug={slug}
-          endPrompt={handleEndPrompt}
-          tags={tags}
-          prompt={prompt}
         />
       )}
     </div>
