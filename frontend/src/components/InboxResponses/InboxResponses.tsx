c06ba1abadabb0781b0001aaf5e5fa891eb27aea
Kyle Coberly <kyle.coberly@gmail.com>
2022-02-28 12:03:34 -0700

    Add inbox count

--- frontend/src/components/InboxResponses/InboxResponses.tsx
+++ frontend/src/components/InboxResponses/InboxResponses.tsx
@@ -7,28 +7,31 @@ type props = {
 };
 
 export default function InboxResponses({ performances }: props) {
+  const promptPerformances = performances.filter(
+    (performance) => performance.type === "prompt"
+  );
   return (
     <div className="InboxResponses">
       <h2>Responses</h2>
+      {promptPerformances.length && (
+        <p className="response-count">Count: {promptPerformances.length}</p>
+      )}
       <ul>
-        {performances.map(
-          (performance) =>
-            performance.type === "prompt" && (
-              <li key={performance.id}>
-                <div className="learner-response">
-                  <Gravatar email={performance.userId} size={60} />
-                  <div>
-                    <div className="username">{performance.userId}:</div>
-                    <AppContent
-                      isContained
-                      className="response-content"
-                      content={performance.payload.response || ""}
-                    />
-                  </div>
-                </div>
-              </li>
-            )
-        )}
+        {promptPerformances.map((performance) => (
+          <li key={performance.id}>
+            <div className="learner-response">
+              <Gravatar email={performance.userId} size={60} />
+              <div>
+                <div className="username">{performance.userId}:</div>
+                <AppContent
+                  isContained
+                  className="response-content"
+                  content={performance.payload.response || ""}
+                />
+              </div>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
