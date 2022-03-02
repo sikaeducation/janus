0a15704c4774c47328473edfd9c9f7062ae0ec2c
Kyle Coberly <kyle.coberly@gmail.com>
2022-03-01 17:49:41 -0700

    Remove link from toast

--- frontend/src/components/ToastNotification/ToastNotification.tsx
+++ frontend/src/components/ToastNotification/ToastNotification.tsx
@@ -1,10 +1,9 @@
-import { Link } from "react-router-dom";
 import "./ToastNotification.scss";
 
 function ToastNotification() {
   return (
     <div className="ToastNotification">
-      <Link to="/activity">You have a new message</Link>
+      <p>New activity!</p>
     </div>
   );
 }
