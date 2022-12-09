4325db7016e335b903a39609273dbaf03bc63418
Kyle Coberly <kyle.coberly@gmail.com>
2022-12-09 13:17:08 -0700

    Add typography demo

---
+++ frontend/src/components/ui/TypographyDemo/TypographyDemo.stories.tsx
@@ -0,0 +1,8 @@
+import TypographyDemo from "./TypographyDemo";
+
+export default {
+  title: "Design/Typography",
+  component: TypographyDemo,
+};
+
+export const Default = () => <TypographyDemo />;
---
+++ frontend/src/components/ui/TypographyDemo/index.ts
@@ -0,0 +1,3 @@
+import Typography from "./TypographyDemo";
+
+export default Typography;
