48a40463e2a330214370f3adfd625a4b538bbdcf
Kyle Coberly <kyle.coberly@gmail.com>
2022-12-10 02:18:01 -0700

    Refactor Activity View

--- frontend/src/views/ActivityManagerView/ActivityManagerView.tsx
+++ frontend/src/views/ActivityManagerView/ActivityManagerView.tsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import "./ActivityManagerView.scss";
 import { useAuth0 } from "@auth0/auth0-react";
 import ActivityIcon from "../../components/ui/ActivityIcon";
 import ModalView from "../ModalView";
 import NewActivityForm from "../NewActivityForm";
+import DataTable from "../../components/ui/DataTable";
+import Button from "../../components/ui/Button";
+import Heading from "../../components/ui/Heading";
+import Icon from "../../components/ui/Icon";
 
 const getActivities = (token: string) => {
   const url = `${process.env.REACT_APP_ACTIVITY_SERVICE_BASE_URL}/activities`;
@@ -19,11 +23,60 @@ const activityTypes = {
   Article: <ActivityIcon activityType="Article" />,
 };
 
+const fields = [
+  {
+    header: "Type",
+    key: "type",
+    proportion: {
+      large: "5%",
+    },
+  },
+  {
+    header: "Live",
+    key: "publishedIcon",
+    proportion: {
+      large: "5%",
+    },
+  },
+  {
+    header: "Name",
+    key: "title",
+    proportion: {
+      large: "40%",
+      small: "100%",
+    },
+  },
+  {
+    header: "Description",
+    key: "description",
+    proportion: {
+      large: "50%",
+    },
+  },
+];
+
+type FormattedActivity = Activity & {
+  id: string;
+  type: NonNullable<ReactNode>;
+  publishedIcon?: ReactNode;
+};
+
 export default function ActivityManagerView() {
   const [activities, setActivities] = useState<Activity[]>([]);
   const activitiesCount = activities.length;
-  const [newActivityOpen, setNewActivityOpen] = useState<boolean>(false);
+  const [newActivityOpen, setNewActivityOpen] = useState(false);
   const { getAccessTokenSilently } = useAuth0();
+
+  const formattedActivities: FormattedActivity[] = activities.map(
+    (activity) => {
+      return {
+        ...activity,
+        id: activity._id || "",
+        type: activityTypes[activity._type],
+        publishedIcon: activity.published ? <Icon type="checkmark" /> : null,
+      };
+    }
+  );
 
   useEffect(() => {
     getAccessTokenSilently({
@@ -86,31 +139,18 @@ export default function ActivityManagerView() {
         </ModalView>
       )}
       <header>
-        <h1>
+        <Heading level={1} margin={false}>
           Activities{" "}
           <span className="activities-count">({activitiesCount})</span>
-        </h1>
-        <button type="button" onClick={handleNewClick}>
+        </Heading>
+        <Button type="primary" action={handleNewClick}>
           New
-        </button>
+        </Button>
       </header>
-      <div className="data-table" role="grid">
-        <div role="row" className="table-row table-headers">
-          <span className="type">Type</span>
-          <span className="name">Name</span>
-          <span className="description">Description</span>
-          <span className="published">Published</span>
-        </div>
-        {activities &&
-          activities.map(({ _id, _type, title, description, published }) => (
-            <div key={_id} role="row" className="table-row">
-              <span>{activityTypes[_type]}</span>
-              <span>{title}</span>
-              <span>{description}</span>
-              <span>{published ? "Yes" : "No"}</span>
-            </div>
-          ))}
-      </div>
+      <DataTable<FormattedActivity>
+        fields={fields}
+        tableData={formattedActivities}
+      />
     </div>
   );
 }
