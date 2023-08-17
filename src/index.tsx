24de09ac8c06081b1fbb29bdc8725126855f4a8b
Kyle Coberly <kyle.coberly@gmail.com>
2023-08-17 02:35:56 -0600

    Fix typos

--- src/index.tsx
+++ src/index.tsx
@@ -8,33 +8,33 @@ import App from "./App";
 import store from "./store";
 import reportWebVitals from "./reportWebVitals";
 import "@sikaeducation/ui/styles.scss";
-import "@sikaeducation/ui/reset.scss";
+import "@sikaeducation/ui/reset";
 
 if (process.env.NODE_ENV !== "production") {
-	// eslint-disable-next-line
+  // eslint-disable-next-line
 	console.log("Environment:");
-	// eslint-disable-next-line
+  // eslint-disable-next-line
 	console.table(process.env);
 }
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Auth0Provider
-			domain={process.env.REACT_APP_AUTH_ZERO_DOMAIN ?? ""}
-			clientId={process.env.REACT_APP_CLIENT_ID ?? ""}
-			redirectUri={window.location.origin}
-			audience={process.env.REACT_APP_AUTH_ZERO_AUDIENCE}
-			scope="openid"
-		>
-			<ReduxProvider store={store}>
-				<Router>
-					<ScrollToTop />
-					<App />
-				</Router>
-			</ReduxProvider>
-		</Auth0Provider>
-	</React.StrictMode>,
-	document.getElementById("root"),
+  <React.StrictMode>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH_ZERO_DOMAIN ?? ""}
+      clientId={process.env.REACT_APP_CLIENT_ID ?? ""}
+      redirectUri={window.location.origin}
+      audience={process.env.REACT_APP_AUTH_ZERO_AUDIENCE}
+      scope="openid"
+    >
+      <ReduxProvider store={store}>
+        <Router>
+          <ScrollToTop />
+          <App />
+        </Router>
+      </ReduxProvider>
+    </Auth0Provider>
+  </React.StrictMode>,
+  document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
