7b811a917b071f794ff8195f051894a1cdff12f2
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-05 18:13:30 -0600

    Fix linting

--- src/views/NewActivityForm/NewActivityForm.tsx
+++ src/views/NewActivityForm/NewActivityForm.tsx
@@ -1,72 +1,77 @@
 import {
-	ComponentPropsWithoutRef, useState,
+  ComponentPropsWithoutRef, useState,
 } from "react";
 import {
-	Form,
+  Form,
 } from "@sikaeducation/ui";
 import "./NewActivityForm.scss";
 
 import newActivityFields from "./new-activity-fields";
 
 type Props = {
-	cancel: () => void;
-	save: (newActivity: Activity) => void;
+  cancel: () => void;
+  save: (newActivity: Activity) => void;
 };
 
 export default function NewActivityForm({
-	save, cancel,
-}: Props){
-	const [
-		newItem,
-		setNewItem,
-	] = useState<ActivityArticle>({
-		_type: "Article",
-		title: "",
-		post_slug: "",
-		description: "",
-		notes: "",
-		published: false,
-	} as ActivityArticle);
+  save, cancel,
+}: Props) {
+  const [
+    newItem,
+    setNewItem,
+  ] = useState<ActivityArticle>({
+    _type: "Article",
+    title: "",
+    post_slug: "",
+    description: "",
+    notes: "",
+    published: false,
+  } as ActivityArticle);
 
-	const saveAndPublish = (newActivity: ActivityArticle) => {
-		save({
-			...newActivity,
-			published: true,
-		});
-	};
+  const saveAndPublish = (newActivity: ActivityArticle) => {
+    save({
+      ...newActivity,
+      published: true,
+    });
+  };
 
-	const actions: ComponentPropsWithoutRef<typeof Form>["actions"] = [
-		{
-			id: "cancel",
-			label: "Cancel",
-			action: () => cancel(),
-			type: "ghost",
-		},
-		{
-			id: "save",
-			label: "Save",
-			action: () => save(newItem),
-			size: "large",
-			type: "secondary",
-		},
-		{
-			id: "save-and-publish",
-			label: "Save and Publish",
-			action: () => saveAndPublish(newItem),
-			size: "large",
-			type: "primary",
-		},
-	];
+  const actions: ComponentPropsWithoutRef<typeof Form>["actions"] = [
+    {
+      id: "cancel",
+      label: "Cancel",
+      action: () => cancel(),
+      type: "ghost",
+    },
+    {
+      id: "save",
+      label: "Save",
+      action: () => save(newItem),
+      size: "large",
+      type: "secondary",
+    },
+    {
+      id: "save-and-publish",
+      label: "Save and Publish",
+      action: () => saveAndPublish(newItem),
+      size: "large",
+      type: "primary",
+    },
+  ];
 
-	return (
-		<div className="NewActivityForm">
-			<Form
-				heading="Create Activity"
-				fields={newActivityFields}
-				actions={actions}
-				newItem={newItem}
-				setNewItem={(newItem) => setNewItem(newItem as ActivityArticle)}
-			/>
-		</div>
-	);
+  console.log(
+    "a",
+    JSON.stringify(newActivityFields),
+  );
+
+  return (
+    <div className="NewActivityForm">
+      <Form
+        heading="Create Activity"
+        fields={newActivityFields}
+        actions={actions}
+        newItem={newItem}
+        setNewItem={(newItem) => setNewItem(newItem as ActivityArticle)}
+      />
+    </div>
+  );
 }
