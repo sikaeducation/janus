40bf13347755c92702a7cda31c66bc6767f7298f
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-17 04:54:26 -0600

    Fix directory

--- static-server-test.sh
+++ static-server-test.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 npx concurrently -k -s first -n "App,Feature Tests" -c "bgGray.white,auto" \
-	"npx http-server build --port 3000 --silent" \
+	"npx http-server dist --port 3000 --silent" \
 	"npx wait-on tcp:3000 && npm run test:features"
