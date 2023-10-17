72afe90683a466fd960f5479724b5e065c5fdff7
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-16 19:03:23 -0600

    WIP switch to vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,33 +2,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-import postcssNested from "postcss-nested"
-import postcssMergeRules from "postcss-merge-rules"
-import postcssDiscardDuplicates from "postcss-discard-duplicates"
-import postcssClean from "postcss-clean"
+import postcssNested from "postcss-nested";
+import postcssMergeRules from "postcss-merge-rules";
+import postcssDiscardDuplicates from "postcss-discard-duplicates";
 
 export default defineConfig({
-	assetsInclude: ["./public"],
-	plugins: [react()],
-	server: {
-		open: "/index.html",
-	},
-	define: {
-		"process.env": {},
-	},
-	css: {
-		postcss: {
-			plugins: [
-				nested: {},
-				merge - rules: {},
-				"discard-duplicates": {},
-				clean: {}
-			],
-		},
-		preprocessorOptions: {
-			scss: {
-				additionalData: `@use "@sikaeducation/ui/styles" as *;`,
-			},
-		},
-	},
+  assetsInclude: ["./public"],
+  plugins: [react()],
+  server: {
+    open: "/index.html",
+  },
+  define: {
+    "process.env": {},
+  },
+  css: {
+    postcss: {
+      plugins: [postcssNested, postcssMergeRules, postcssDiscardDuplicates],
+    },
+    preprocessorOptions: {
+      scss: {
+        // additionalData: `@use "@sikaeducation/ui/styles" as *;`,
+      },
+    },
+  },
 });
