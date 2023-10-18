fc68b1bcec80c825e6e42a2da3e96678539c3bec
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-18 03:50:45 -0600

    WIP switch to vite

--- src/slices/apiSlice.ts
+++ src/slices/apiSlice.ts
@@ -7,7 +7,7 @@ const { getToken } = tokenAccessors;
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.VITE_ACTIVITY_SERVICE_BASE_URL,
+    baseUrl: import.meta.env.VITE_ACTIVITY_SERVICE_BASE_URL,
     prepareHeaders: async (headers) => {
       const token = await getToken();
 
