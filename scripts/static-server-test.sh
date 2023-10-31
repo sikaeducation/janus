5850c90f0f163266d80ef711d1f412dd9e8e86fb
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-31 15:45:24 -0600

    Fix gh actions

--- scripts/static-server-test.sh
+++ scripts/static-server-test.sh
@@ -3,5 +3,5 @@
 # "npx http-server dist --port 3005 --silent" \
 
 npx concurrently -k -s first -n "App,Feature Tests" -c "bgGray.white,auto" \
-	"npx http-server dist --port 3001" \
-	"npx wait-on tcp:3001 && npm run test:features"
+	"npx http-server dist --port 5173" \
+	"npx wait-on tcp:5173 && npm run test:features"
