af521d080fd8e7987dd9c39bd49fec44ccd8b031
Kyle Coberly <kyle.coberly@gmail.com>
2023-10-31 15:59:42 -0600

    Fix ports

--- scripts/static-server-test.sh
+++ scripts/static-server-test.sh
@@ -3,5 +3,5 @@
 # "npx http-server dist --port 3005 --silent" \
 
 npx concurrently -k -s first -n "App,Feature Tests" -c "bgGray.white,auto" \
-	"npx http-server dist --port 5173" \
-	"npx wait-on tcp:5173 && npm run test:features"
+	"npx http-server dist --port 3005" \
+	"npx wait-on tcp:3005 && npm run test:features"
