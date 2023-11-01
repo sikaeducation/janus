5171997d406cfc74616e6bebb6c9d1cf120d393c
Kyle Coberly <kyle.coberly@gmail.com>
2023-11-01 01:00:22 -0600

    Remove CI

--- scripts/static-server-test.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-npx concurrently -k -s first -n "App,Feature Tests" -c "bgGray.white,auto" \
-	"npx http-server dist --port 3005 --silent" \
-	"npx wait-on tcp:3005 && npm run test:features:ci"
