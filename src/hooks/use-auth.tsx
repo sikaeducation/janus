19bf5a7f39e1c63f48c45ccac3742e8ebfa12b15
Kyle Coberly <kyle.coberly@gmail.com>
2023-11-01 17:33:31 -0600

    Put token setter back in

--- src/hooks/use-auth.tsx
+++ src/hooks/use-auth.tsx
@@ -3,9 +3,11 @@ import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { setUser } from "@/slices/userSlice";
 import { User } from "@/declarations";
+import { setTokenFetcher } from "@/utilities/security";
 
 export default function useAuth() {
-  const { user, isLoading, isAuthenticated } = useAuth0<User>();
+  const { user, isLoading, isAuthenticated, getAccessTokenSilently } =
+    useAuth0<User>();
   const dispatch = useDispatch();
   useEffect(() => {
     if (
@@ -28,4 +30,6 @@ export default function useAuth() {
       );
     }
   }, [isAuthenticated, isLoading, user, dispatch]);
+
+  setTokenFetcher(getAccessTokenSilently);
 }
--- src/utilities/security.ts
+++ src/utilities/security.ts
@@ -4,9 +4,15 @@ type TokenGetter = (options?: GetTokenSilentlyOptions) => Promise<string>;
 let getAccessTokenSilently: TokenGetter;
 
 export const getToken = () => {
-	if (import.meta.env.MODE === "test") {
-		return Promise.resolve("fake-token");
-	} else {
-		return getAccessTokenSilently();
-	}
+  if (import.meta.env.MODE === "test") {
+    return Promise.resolve("fake-token");
+  } else {
+    if (!getAccessTokenSilently) throw new Error("Authentication error");
+
+    return getAccessTokenSilently();
+  }
+};
+
+export const setTokenFetcher = (accessTokenSetter: TokenGetter) => {
+  getAccessTokenSilently = accessTokenSetter;
 };
